0d81f719ce001432ccf10a2117ba4c0db4e357dd
dl0461 <daniellicari0461@gmail.com>
2023-10-12 19:23:33 -0500

    rebase-root

--- .config/shell/wayland-config/zprofile.sh
+++ .config/shell/wayland-config/zprofile.sh
@@ -3,12 +3,16 @@ autoload -U add-zsh-hook
 if [ -f "$CFG/shell/sway-config/cmdpatterns" ]; then
 	shellhistoryfilter_hook() {
 		shellhistoryfilter.py "$CFG/shell/sway-config/cmdpatterns"\
-			&> "$XDG_STATE_HOME/zsh/shellhistoryfilter_hook"
+			&> "$XDG_STATE_HOME/zsh-shellhistoryfilter-hook-log"
 	}
 
 	add-zsh-hook zshexit shellhistoryfilter_hook
 fi
 
+if [ -f "$HOME/.ssh/github" ]; then
+	ssh-add "$HOME/.ssh/github" &> /dev/null
+fi
+
 if [ "$(tty)" = '/dev/tty1' ]; then
-	exec sway
+	exec sway &> "$XDG_STATE_HOME/sway-log"
 fi
