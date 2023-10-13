b3e0efc930e706d409b792cdadd17ff464161bfd
dl0461 <daniellicari0461@gmail.com>
2023-10-13 16:23:59 -0500

    ssh-add-home

--- .config/shell/wayland-config/zprofile.sh
+++ .config/shell/wayland-config/zprofile.sh
@@ -5,12 +5,15 @@ if [ -f "$CFG/shell/sway-config/cmdpatterns" ]; then
 		shellhistoryfilter.py "$CFG/shell/sway-config/cmdpatterns"\
 			&> "$XDG_STATE_HOME/zsh-shellhistoryfilter-hook-log"
 	}
-
 	add-zsh-hook zshexit shellhistoryfilter_hook
 fi
 
 if [ -f "$HOME/.ssh/github" ]; then
 	ssh-add "$HOME/.ssh/github" &> /dev/null
+fi
+
+if [ -f "$HOME/.ssh/home" ]; then
+	ssh-add "$HOME/.ssh/home" &> /dev/null
 fi
 
 if [ "$(tty)" = '/dev/tty1' ]; then
