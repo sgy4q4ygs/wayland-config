7469e1773e134cf1e2d654f8f595df2c9fa9e730
dl0461 <daniellicari0461@gmail.com>
2023-10-13 16:48:15 -0500

    remove-ssh-add

--- .config/shell/wayland-config/zprofile.sh
+++ .config/shell/wayland-config/zprofile.sh
@@ -8,14 +8,6 @@ if [ -f "$CFG/shell/sway-config/cmdpatterns" ]; then
 	add-zsh-hook zshexit shellhistoryfilter_hook
 fi
 
-if [ -f "$HOME/.ssh/github" ]; then
-	ssh-add "$HOME/.ssh/github" &> /dev/null
-fi
-
-if [ -f "$HOME/.ssh/home" ]; then
-	ssh-add "$HOME/.ssh/home" &> /dev/null
-fi
-
 if [ "$(tty)" = '/dev/tty1' ]; then
 	exec sway &> "$XDG_STATE_HOME/sway-log"
 fi
