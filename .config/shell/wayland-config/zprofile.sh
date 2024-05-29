9eae3d806c9b7793a98694233023ffc2d16409d8
dl0461 <daniellicari0461@gmail.com>
2024-05-28 20:09:24 -0500

    sway-screencasting-in-general

--- .config/shell/wayland-config/zprofile.sh
+++ .config/shell/wayland-config/zprofile.sh
@@ -11,5 +11,10 @@ if [ -f "$CFG/shell/sway-config/cmdpatterns" ]; then
 fi
 
 if which sway && [ "$(tty)" = '/dev/tty1' ]; then
+# screen casting
+# systemctl --user show-environment
+# should show: XDG_CURRENT_DESKTOP=sway
+    export XDG_CURRENT_DESKTOP=sway
+    systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 	exec sway &> "$XDG_STATE_HOME/sway-log"
 fi
