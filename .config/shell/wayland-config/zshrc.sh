c4e6fee70f4cacad98ceb87d5f58b538008fcc28
dl0461 <daniellicari0461@gmail.com>
2024-07-21 19:56:17 -0500

    colors

--- .config/shell/wayland-config/zshrc.sh
+++ .config/shell/wayland-config/zshrc.sh
@@ -1,13 +1,13 @@
 _dotnet()
 {
-  local completions=("$(dotnet complete "$words")")
-  if [ -z "$completions" ]; then
-    _arguments '*::arguments: _normal'
-    return
-  fi
-  local -a completionArray
-  completionArray=("${(@ps:\n:)completions}") # Split by newline
-  _values 'dotnet subcommands' $completionArray
+    local completions=("$(dotnet complete "$words")")
+    if [ -z "$completions" ]; then
+        _arguments '*::arguments: _normal'
+        return
+    fi
+    local -a completionArray
+    completionArray=("${(@ps:\n:)completions}") # Split by newline
+    _values 'dotnet subcommands' $completionArray
 }
 
 compdef _dotnet dotnet
